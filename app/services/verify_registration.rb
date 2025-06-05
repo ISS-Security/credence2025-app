ISS-Security/credence2022-app

Sign non-authenticated requests to API

app/services/verify_registration.rb
@@ -17,8 +17,11 @@ module Credence
       registration_data['verification_url'] =
         "#{@config.APP_URL}/auth/register/#{registration_token}"
 
-      response = HTTP.post("#{@config.API_URL}/auth/register",
-                           json: registration_data)
+      response = HTTP.post(
+        "#{@config.API_URL}/auth/register",
+        json: SignedMessage.sign(registration_data)
+      )
+
       raise(VerificationError) unless response.code == 202
 
       JSON.parse(response.to_s)

ISS-Security/credence2025-app

app/services/verify_registration.rb
@@ -18,8 +18,11 @@ module Credence
       reg_details['verification_url'] =
         "#{@config.APP_URL}/auth/register/#{registration_token}"
 
-      response = HTTP.post("#{@config.API_URL}/auth/register",
-                           json: reg_details)
+      response = HTTP.post(
+        "#{@config.API_URL}/auth/register",
+json: SignedMessage.sign(reg_details)
+      )
+
       raise(VerificationError) unless response.code == 202
 
       JSON.parse(response.to_s)