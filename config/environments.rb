ISS-Security/credence2022-app

Configure browser-side CSPs and security headers

config/environments.rb
@@ -16,28 +16,25 @@ module Credence
 
     # Environment variables setup
     Figaro.application = Figaro::Application.new(
-      environment:,
+      environment: environment,
       path: File.expand_path('config/secrets.yml')
     )
     Figaro.load
-    def self.config = Figaro.env
+    def self.config() = Figaro.env
 
     # Logger setup
     LOGGER = Logger.new($stderr)
-    def self.logger = LOGGER
+    def self.logger() = LOGGER
 
     ONE_MONTH = 30 * 24 * 60 * 60
 
     configure do
+      SecureSession.setup(ENV['REDIS_TLS_URL']) # REDIS_TLS_URL used again below
       SecureMessage.setup(ENV.delete('MSG_KEY'))
       SignedMessage.setup(config)
     end
 
     configure :production do
-      SecureSession.setup(ENV.fetch('REDIS_TLS_URL')) # REDIS_TLS_URL used again below
-
-      use Rack::SslEnforcer, hsts: true
-
       use Rack::Session::Redis,
           expire_after: ONE_MONTH,
           httponly: true,
@@ -49,16 +46,17 @@ module Credence
     end
 
     configure :development, :test do
-      require 'pry'
-
-      # NOTE: env var REDIS_URL only used to wipe the session store (ok to be nil)
-      SecureSession.setup(ENV.fetch('REDIS_URL', nil)) # REDIS_URL used again below
-
       # use Rack::Session::Cookie,
-      #     expire_after: ONE_MONTH, secret: config.SESSION_SECRET
+      #     expire_after: ONE_MONTH,
+      #     secret: config.SESSION_SECRET,
+      #     httponly: true,
+      #     same_site: :strict
+
 
       use Rack::Session::Pool,
-          expire_after: ONE_MONTH
+          expire_after: ONE_MONTH,
+          httponly: true,
+          same_site: :strict
 
       # use Rack::Session::Redis,
       #     expire_after: ONE_MONTH,
@@ -73,7 +71,9 @@ module Credence
       require 'pry'
 
       # Allows running reload! in pry to restart entire app
-      def self.reload! = exec 'pry -r ./spec/test_load_all'
+      def self.reload!
+        exec 'pry -r ./spec/test_load_all'
+      end
     end
   end
 end

ISS-Security/credence2025-app

config/environments.rb
@@ -4,7 +4,6 @@ require 'delegate'
 require 'roda'
 require 'figaro'
 require 'logger'
-require 'rack/ssl-enforcer'
 require 'rack/session'
 require 'rack/session/redis'
 require_relative '../require_app'
@@ -18,11 +17,11 @@ module Credence
 
     # Environment variables setup
     Figaro.application = Figaro::Application.new(
-      environment:,
+      environment: environment,
       path: File.expand_path('config/secrets.yml')
     )
     Figaro.load
-    def self.config = Figaro.env
+    def self.config() = Figaro.env
 
     # HTTP Request logging
     configure :development, :production do
@@ -31,7 +30,7 @@ module Credence
 
     # Custom events logging
     LOGGER = Logger.new($stderr)
-    def self.logger = LOGGER
+    def self.logger() = LOGGER
 
     # Allows binding.pry in dev/test and rake console in production
     require 'pry'
@@ -47,21 +46,25 @@ module Credence
       # Suppresses log info/warning outputs in dev/test environments
       logger.level = Logger::ERROR
 
-      # NOTE: env var REDIS_URL only used to wipe the session store (ok to be nil)
-      SecureSession.setup(ENV.fetch('REDIS_URL', nil)) # REDIS_URL used again below
-
       # use Rack::Session::Cookie,
-      #     expire_after: ONE_MONTH, secret: config.SESSION_SECRET
+      #     secret: config.SESSION_SECRET,
+      #     expire_after: ONE_MONTH,
+      #     httponly: true,
+      #     same_site: :lax
 
       use Rack::Session::Pool,
-          expire_after: ONE_MONTH
+          expire_after: ONE_MONTH,
+          httponly: true,
+          same_site: :lax
 
       # use Rack::Session::Redis,
       #     expire_after: ONE_MONTH,
       #     redis_server: @redis_url
 
       # Allows running reload! in pry to restart entire app
-      def self.reload! = exec 'pry -r ./spec/test_load_all'
+      def self.reload!
+        exec 'pry -r ./spec/test_load_all'
+      end
     end
 
     configure :production do